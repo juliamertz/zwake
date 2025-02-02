b37ecaa56322c93b18abd38ca9de1f014a5f16d2
Julia Mertz <info@juliamertz.dev>
2025-02-02 18:37:19 +0100

    add `port` cli flag

--- src/main.zig
+++ src/main.zig
@@ -2,24 +2,29 @@ const std = @import("std");
 const net = @import("net.zig");
 const clap = @import("clap");
 
+const parsers = .{
+    .string = clap.parsers.string,
+    .port = clap.parsers.enumeration(net.Port),
+};
+
+const params = clap.parseParamsComptime(
+    \\-h, --help            Display this help and exit.
+    \\-m, --mac  <string>   48-bit mac address
+    \\-i, --ip   <string>   Broadcast IP address
+    \\-p, --port <port>     Port to target, one of: [discard, echo, reserved]
+);
+
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
 
-    const params = comptime clap.parseParamsComptime(
-        \\-h, --help           Display this help and exit.
-        \\-m, --mac <string>   48-bit mac address
-        \\-i, --ip  <string>   Broadcast IP address
-    );
-
     const stderr = std.io.getStdErr().writer();
-
     var diag = clap.Diagnostic{};
     const parse_options = clap.ParseOptions{
         .allocator = gpa.allocator(),
         .diagnostic = &diag,
     };
-    var res = clap.parse(clap.Help, &params, clap.parsers.default, parse_options) catch |err| {
+    var res = clap.parse(clap.Help, &params, parsers, parse_options) catch |err| {
         diag.report(stderr, err) catch {};
         return err;
     };
@@ -34,11 +39,11 @@ pub fn main() !void {
     const socket = try net.openSocket();
     defer std.posix.close(socket);
 
-    const port = @intFromEnum(net.Port.discard);
-    const address = try std.net.Address.parseIp4(res.args.ip.?, port);
-
     const mac = try net.MacAddress.parse(res.args.mac.?);
     const packet = net.MagicPacket.init(mac.addr);
 
-    try packet.broadcast(socket, address);
+    const port = res.args.port or net.Port.discard;
+    const ip = try std.net.Address.parseIp4(res.args.ip.?, @intFromEnum(port));
+
+    try packet.broadcast(socket, ip);
 }
